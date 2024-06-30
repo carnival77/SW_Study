b92cfbda72762dbbb78bfe9e0734425d04a42180
junha <yy4124@gmail.com>
2024-07-01 01:04:39 +0900

    디렉토리 변경

--- Algorithm Study/JunhaJang/20240623/토스트 계란틀.cpp
+++ Algorithm Study/JunhaJang/20240623/토스트 계란틀.cpp
@@ -1,102 +1,164 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 #include <vector>
+#include <string>
+#include <cmath>
+#include <stack>
 #include <queue>
+#include <tuple>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
 #define ENDL '\n'
 
 using namespace std;
 using ll = long long;
 
-int dRow[4] = { -1, 1, 0, 0 };
-int dCol[4] = { 0, 0, -1, 1 };
+#define EMPTY 0
+#define ENEMY 6
+#define MARK true
+#define ERASE false
 
-int answer = 0;
-int boardSize;
-int minBound, maxBound;
+enum direction {
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT
+};
+
+int minUnreached = INT_MAX;
+int boardRow, boardCol;
 vector<vector<int>>board;
-vector<vector<int>>basicCellNum;
+vector<vector<bool>>reached;
+vector<tuple<int, int, int>>pieces; //row, col, kind
+
+direction getLeft(direction dir) {
+	if (dir == UP) return LEFT;
+	return dir - 1;
+}
+direction getRight(direction dir) {
+	if (dir == LEFT) return UP;
+	return dir + 1;
+}
+direction getBackward(direction dir) {
+	if (dir == DOWN) return UP;
+	if (dir == LEFT) return RIGHT;
+	return dir + 2;
+}
+
+pair<int, int> getNextPos(int row, int col, direction dir) {
+	switch (dir) {
+	case UP:
+		row--;
+		break;
+	case RIGHT:
+		col++;
+		break;
+	case DOWN:
+		row++;
+		break;
+	case LEFT:
+		col--;
+		break;
+	}
+	return { row, col };
+}
 
 bool isValidRange(int row, int col) {
-	if (0 <= row && row < boardSize && 0 <= col && col < boardSize) return true;
+	if (0 <= row && row < boardRow && 0 <= col && col < boardCol) return true;
 	return false;
 }
 
-bool canMove(int row, int col, int nextRow, int nextCol) {
-	int diff = abs(board[row][col] - board[nextRow][nextCol]);
-	if (minBound <= diff && diff <= maxBound) return true;
-	return false;
-}
+void affect(int row, int col, int kind, direction dir, bool mode) {
+	vector<direction>availableDir;
+	switch (kind) {
+	case 1:
+		availableDir.push_back(dir);
+		break;
+	case 2:
+		availableDir.push_back(getLeft(dir));
+		availableDir.push_back(getRight(dir);
+		break;
+	case 3:
+		availableDir.push_back(dir);
+		availableDir.push_back(getRight(dir);
+		break;
+	case 4:
+		availableDir.push_back(dir);
+		availableDir.push_back(getLeft(dir));
+		availableDir.push_back(getRight(dir);
+		break;
+	case 5:
+		availableDir.push_back(dir);
+		availableDir.push_back(getLeft(dir));
+		availableDir.push_back(getRight(dir);
+		availableDir.push_back(getBackward(dir));
+		break;
+	}
 
-bool gamePlay() {
-	bool isChange = false;
-	vector<vector<bool>>visited(boardSize, vector<bool>(boardSize, false));
-	vector<vector<int>>cellNumBoard(basicCellNum);
-	
-	for (int i = 0; i < boardSize; i++) {
-		for (int j = 0; j < boardSize; j++) {
-			if (visited[i][j]) continue;
+	for (direction dir : availableDir) {
+		int tmpRow = row;
+		int tmpCol = col;
 
-			int cellSum = 0;
-			int cellCount = 0;
-			int tmpCellNum = basicCellNum[i][j];
+		while (isValidRange(tmpRow, tmpCol)) {
+			if (board[tmpRow][tmpCol] == ENEMY) break;
 
-			queue<pair<int, int>>q;
-			queue<pair<int, int>>willChange;
-			q.push({ i,j });
+			reached[tmpRow][tmpCol] = mode;
 
-			while (!q.empty()) {
-				auto [row, col] = q.front();
-				q.pop();
-
-				if (visited[row][col]) continue;
-				visited[row][col] = true;
-				cellNumBoard[row][col] = tmpCellNum;
-
-				willChange.push({ row, col });
-				cellSum += board[row][col];
-				cellCount++;
-
-				for (int i = 0; i < 4; i++) {
-					int nextRow = row + dRow[i];
-					int nextCol = col + dCol[i];
-					if (isValidRange(nextRow, nextCol) && visited[nextRow][nextCol] == false) {
-						if (canMove(row, col, nextRow, nextCol)) {
-							isChange = true;
-							q.push({ nextRow, nextCol });
-						}
-					}
-				}
-			}
-
-			int changeNum = cellSum / cellCount;
-			while (!willChange.empty()) {
-				auto [row, col] = willChange.front();
-				willChange.pop();
-				board[row][col] = changeNum;
-			}
+			auto [nextRow, nextCol] = getNextPos(tmpRow, tmpCol, dir);
+			tmpRow = nextRow;
+			tmpCol = nextCol;
 		}
 	}
-	
-	return isChange;
+}
+
+void markReachable(int row, int col, int kind, direction dir) {
+	affect(row, col, kind, dir, MARK);
+}
+
+void eraseReachable(int row, int col, int kind, direction dir) {
+	affect(row, col, kind, dir, ERASE);
+}
+
+void dfs(int pieceIdx) {
+	if (pieceIdx == pieces.size()) {
+		int cnt = 0;
+		for (int i = 0; i < boardRow; i++) {
+			for (int j = 0; j < boardCol; j++) {
+				if (reached[i][j]) cnt++;
+			}
+		}
+		int emptyCnt = boardRow * boardCol - cnt;
+		minUnreached = min(minUnreachedm, emptyCnt);
+		return;
+	}
+
+	auto [row, col, kind] = pieces[pieceIdx];
+	for (direction dir = UP; dir <= LEFT; dir++) {
+		markReachable(row, col, kind, dir);
+		dfs(idx + 1);
+		eraseReachable(row, col, kind, dir);
+
+		if (kind == 5) return;
+	}
 }
 
 int main(void) {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> boardSize >> minBound >> maxBound;
-	board.resize(boardSize, vector<int>(boardSize));
-	basicCellNum.resize(boardSize, vector<int>(boardSize));
-
-	int cellCount = 0;
-	for (int i = 0; i < boardSize; i++) {
-		for (int j = 0; j < boardSize; j++) {
-			cin >> board[i][j];
-			basicCellNum[i][j] = cellCount++;
+	cin >> boardRow >> boardCol;
+	board.resize(boardRow, vector<int>(boardCol));
+	reached.resize(boardRow, vector<int>(boardCol, false));
+	for (int i = 0; i < boardRow; i++) {
+		for (int j = 0; j < boardCol; j++) {
+			int piece; cin >> board[i][j];
+			if (0 < piece && piece < 6) pieces.push_back({i, j, piece});
 		}
 	}
 
-	while (gamePlay()) answer++;
+	dfs(0);
 
-	cout << answer;
-
+	cout << minUnreached;
 	return 0;
 }
